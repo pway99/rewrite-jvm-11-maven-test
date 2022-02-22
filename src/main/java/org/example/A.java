c5cd7b966ebeefd8ac32fe0776efdbe3c05e2645
Patrick <patway99@gmail.com>
2022-02-22 10:29:07 -0800

    testing change package

--- src/main/java/org/example/A.java
+++ src/main/java/org/example/A.java
@@ -1,10 +1,6 @@
 package org.example;
 
 public class A {
-    private static int anInt;
     public static class B {
-        public int getInt() {
-            return anInt;
-        }
     }
 }
--- src/main/java/org/example/C.java
+++ src/main/java/org/example/C.java
@@ -3,7 +3,4 @@ package org.example;
 public class C {
     A a = new A();
     A.B b = new A.B();
-    void doSomething() {
-        int i = b.getInt();
-    }
 }
