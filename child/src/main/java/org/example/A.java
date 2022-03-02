12b314fd8ff06a69481d104a09374e0d097220dc
Patrick <patway99@gmail.com>
2022-03-02 12:25:52 -0800

    making it a bit more complex

--- child/src/main/java/org/example/A.java
+++ child/src/main/java/org/example/A.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class A {
+    C c = new C();
+    public void doSomething() {
+        c.goFish(0, "Heart");
+    }
+
     public static class B {
         String s = "HellWorld, please tell your humans to be kind";
+
     }
 }
--- child/src/main/java/org/example/C.java
+++ child/src/main/java/org/example/C.java
@@ -3,4 +3,12 @@ package org.example;
 public class C {
     A a = new A();
     A.B b = new A.B();
+
+    public void goFish(Integer number, String type) {
+
+    }
+
+    public void goFish(String type, Integer number) {
+
+    }
 }
